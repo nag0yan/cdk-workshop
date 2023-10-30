cea41e30300ab779abd8a194e743369a9e9e71ca
nag0yan <matsui-ka@kadokawa.jp>
2023-10-30 23:47:33 +0900

    hit counter

---
+++ lib/hitcounter.ts
@@ -0,0 +1,41 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+
+export interface HitCounterProps {
+  /** the function for which we want to count url hits **/
+  downstream: lambda.IFunction;
+}
+
+export class HitCounter extends Construct {
+   /** allows accessing the counter function **/
+  public readonly handler: lambda.Function;
+
+  constructor(scope: Construct, id: string, props: HitCounterProps){
+    super(scope, id);
+   
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: {
+        name: 'path',
+        type: dynamodb.AttributeType.STRING
+      }
+    });
+
+    this.handler = new lambda.Function(this, 'HitCouterHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hitcounter.handler',
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
+
+    // grant the lambda role read/write permissions to our table
+    table.grantReadWriteData(this.handler);
+
+    // grant the lambda role invoke permissions to our downstream function
+    props.downstream.grantInvoke(this.handler);
+  }
+}
