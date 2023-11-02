b6ca4f147a561730af3556e510946c5c613b6292
nag0yan <matsui-ka@kadokawa.jp>
2023-11-02 18:55:19 +0900

    assertion & validation test

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -4,8 +4,17 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export interface HitCounterProps {
-  /** the function for which we want to count url hits **/
+  /** the function for which we want to count url hits */
   downstream: lambda.IFunction;
+
+  /**
+   * The read capcacity units for the table
+   * 
+   * Must be greater than 5 and lower than 20
+   * 
+   * @default 5
+   */
+  readCapacity?: number;
 }
 
 export class HitCounter extends Construct {
@@ -16,6 +25,10 @@ export class HitCounter extends Construct {
   public readonly table: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props: HitCounterProps){
+    if (props.readCapacity !== undefined && (props.readCapacity < 5 || props.readCapacity > 20)){
+      throw new Error('readCapacity must be greater than 5 and less than 20');
+    }
+    
     super(scope, id);
    
     const table = new dynamodb.Table(this, 'Hits', {
@@ -23,9 +36,10 @@ export class HitCounter extends Construct {
         name: 'path',
         type: dynamodb.AttributeType.STRING
       },
-      removalPolicy: cdk.RemovalPolicy.DESTROY
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      readCapacity: props.readCapacity ?? 5
     });
-    this.table = table;
 
     this.handler = new lambda.Function(this, 'HitCouterHandler', {
       runtime: lambda.Runtime.NODEJS_16_X,
